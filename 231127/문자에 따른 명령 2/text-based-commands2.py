0d1c9308b7a682bbfdaad2bd58568bd52185c48d
Sol <121751626+s01k1m@users.noreply.github.com>
2023-11-27 01:17:44 +0900

    Title: 문자에 따른 명령 2, Time: 122ms, Memory: 26MB, Status: Passed - CodeTree

--- 231127/문자에 따른 명령 2/text-based-commands2.py
+++ 231127/문자에 따른 명령 2/text-based-commands2.py
@@ -5,7 +5,7 @@ L 왼쪽 R 오른쪽 F forward
 arr = list(input())
 # 시계방향 북 동 남 서
 dx = [0, 1, 0, -1]
-dy = [-1, 0, 1, 0]
+dy = [+1, 0, -1, 0]
 
 cur = 0
 x, y = 0,0
@@ -15,6 +15,9 @@ for i in arr:
     if i == "R":
         cur += 1
     if i == "F":
+        if cur < 0:
+            cur = abs(cur) % 4
+            cur = -cur
         cur %= 4
         x, y = x+dx[cur], y+dy[cur]
 
