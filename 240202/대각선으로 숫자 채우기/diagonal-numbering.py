06ac92e3b5cc690bf565718254150b7ab200bb1b
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-02 14:05:20 +0900

    Title: 대각선으로 숫자 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240202/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -0,0 +1,37 @@
+# 00 / 01 10 / 02 11 20 / 03 12 22 30 / ... / 25 34 / 35
+# 4 * 6
+
+row, col = map(int, input().split()) # 3 3
+arr = [
+    [0] * col
+    for _ in range(row)
+]
+n = 1
+
+for j in range(0, col): # 3 -> 0 1 2  
+    for i in range(0, j+1): # 0 / 1 0 / 2 1 0 /
+        arr[i][j-i] = n # 1 2 2 1 / 2 2 
+        n += 1
+
+# for j in range(1, col): # 1 2
+#     for i in range(col-1, j-1, -1): # 1-> 1 2 2-> 2 
+#         # arr[i][j] # 2 1
+#         # print(i, col-j)
+#         arr[][i] = n
+#         n += 1
+
+for start_row in range(1, row):
+    curr_row = start_row
+    curr_col = col - 1
+
+    while 0 <= curr_col < col  and 0< curr_row < row:
+        arr[curr_row][curr_col] = n
+        curr_row += 1
+        curr_col -= 1
+        n += 1
+        
+
+for i in range(row):
+    for j in range(col):
+        print(arr[i][j], end=" ")
+    print()
