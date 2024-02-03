aa582e8c972ef8f1c4607e4936a36055ebb436fc
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-04 00:04:50 +0900

    Title: 10진수로 변환하기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240204/10진수로 변환하기/convert-to-decimal.py
+++ 240204/10진수로 변환하기/convert-to-decimal.py
@@ -1,7 +1,8 @@
-binary = list(map(int, input().split()))
+binary = list(map(int, list(input())))
+# print(binary)
 num = 0
-
-for i in range(5):
-    num = num * 2 + binary[i-1]
+# print(len(binary))
+for i in range(len(binary)):
+    num = num * 2 + binary[i]
 
 print(num)
