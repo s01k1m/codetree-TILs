9666a89b27139792097328a3c303d00272ddb72e
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-04 00:11:26 +0900

    Title: 최대공약수 구하기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240204/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,10 @@
+n, m =map(int, input().split())
+
+x = int(min(n, m)**1/2)
+greatestCommoDivision = 0
+for i in range(1, x+1):
+    if n % i == 0 and m % i == 0:
+        greatestCommoDivision = i
+    
+
+print(greatestCommoDivision)
