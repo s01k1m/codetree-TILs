f40b322dff66886a35f86048dc53924e973ec46c
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-05 18:21:41 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 240205/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -1,14 +1,10 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
+def max_v(a):
+    if a == 0:
+        return arr[0]
 
-def f(n, big, arr):
-    if n >= len(arr):
-        return big
-    if arr[n] > big:
-        big = arr[n]
-    
-    return f(n+1, big, arr)
-    
+    return max(max_v(a-1), arr[a])
 
-print(f(0, arr[0], arr))
+print(max_v(n-1))
