76e50d78f6a001544374c650094bde07d3e9044c
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-05 19:04:17 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240205/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,17 +1,17 @@
 def lcd(n):
-    if n == 1 :
-        return arr[1] * arr[0] / gcd(arr[0], arr[1])
-    return arr[n] * lcd(n-1) / gcd(arr[n], lcd(n-1))
+    if n == 0:
+        return arr[0]
+    if n == 1:
+        return arr[0] * arr[1] / gcd(arr[0], arr[1])
+    return arr[n] * lcd(n-1) / gcd(lcd(n-1), arr[n])
 
 n = int(input())
 arr = list(map(int, input().split()))
 
-
-def gcd(a ,b):
-
+def gcd(a, b):
     if a % b == 0:
         return b
     else:
-        return gcd(b, a%b)
-    
+        return gcd(b, a % b)
+
 print(int(lcd(n-1)))
