63114ad3f30a8d407f7a4346ad1b7887c861c3b5
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-07 15:19:20 +0900

    Title: 코드네임, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/코드네임/code-name.py
+++ 240207/코드네임/code-name.py
@@ -11,6 +11,12 @@ for _ in range(5):
     b = int(b)
     agents.append(Agent(a,b))
 
-agents = sorted(agents, key=lambda x: x.score)
+# agents = sorted(agents, key=lambda x: x.score)
+# print(agents[0].name, agents[0].score)
 
-print(agents[0].name, agents[0].score)
+min_idx = 0
+for i in range(1, 5):
+    if agents[min_idx].score > agents[i].score:
+        min_idx = i
+    
+print(agents[min_idx].name, agents[min_idx].score)
