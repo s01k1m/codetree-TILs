c699eec8f3041fefde664a1637c84d09d433af22
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-07 15:40:35 +0900

    Title: 상품 코드, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/상품 코드/product-code.py
+++ 240207/상품 코드/product-code.py
@@ -1,11 +1,12 @@
 class ProductCode:
-    def __init__(self, name, code):
+    def __init__(self, name="codetree", code="50"):
         self.name = name
         self.code = code
 
 name_i, code_i = input().split()
+a = ProductCode()
+b = ProductCode(name_i, int(code_i))
 
-a = ProductCode(name_i, int(code_i))
 
-print(f"product 50 is codetree")
-print(f"prodcut {a.code} is {a.name}")
+print(f"product {a.code} is {a.name}")
+print(f"product {b.code} is {b.name}")
