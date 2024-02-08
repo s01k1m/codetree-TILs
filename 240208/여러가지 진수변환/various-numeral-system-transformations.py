7b5a2088099a8517d155fc0b0407ba18f38f15ec
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-08 23:47:40 +0900

    Title: 여러가지 진수변환, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 240208/여러가지 진수변환/various-numeral-system-transformations.py
+++ 240208/여러가지 진수변환/various-numeral-system-transformations.py
@@ -1,11 +1,11 @@
 N, B = map(int, input().split())
 digit = []
 
-while N >= 4:
-    digit.append(N % 4)
-    N = N // 4
+while N >= B:
+    digit.append(N % B)
+    N = N // B
 
-if N < 4:
+if N < B:
     digit.append(N)
 for i in digit[::-1]:
     print(i, end="")
