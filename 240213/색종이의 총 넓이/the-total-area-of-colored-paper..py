0a7087bec63216dc0824d06606f47f44d1965314
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-13 00:02:16 +0900

    Title: 색종이의 총 넓이, Time: 130ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240213/색종이의 총 넓이/the-total-area-of-colored-paper..py
@@ -0,0 +1,19 @@
+n = int(input())
+arr= [[0]*201 for _ in range(201)]
+
+
+def square(a, b):
+    for x in range(a, (a-8), -1):
+        for y in range(b, (a+8)):
+            arr[x][y] = 1
+
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    square(a+100, b+100)
+
+ans = 0
+for _ in(arr):
+    ans += sum(_)
+
+print(ans)
