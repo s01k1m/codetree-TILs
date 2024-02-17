366cac7f4267e5dbc68d249b5072e32ca738fbb9
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-18 00:14:01 +0900

    Title: 좌우로 움직이는 로봇, Time: 337ms, Memory: 42MB, Status: Passed - CodeTree

--- 240218/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240218/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -17,7 +17,7 @@ for i in range(a):
             n += 1
     
     if i == a-1: # 멈추고 난 시간 이후 기록
-        for j in range(n, 50001):
+        for j in range(n, 1000001):
             arr_A[j] = arr_A[n-1]
 
 n = 1
@@ -34,13 +34,13 @@ for i in range(b):
             n += 1
     
     if i == b-1: # 멈추고 난 시간 이후 기록
-        for j in range(n, 50001):
+        for j in range(n, 1000001):
             arr_B[j] = arr_B[n-1]
 
 ans = 0
-for i in range(1, 50001):
+for i in range(1, 1000001):
     if arr_A[i] == arr_B[i]:
-        if (arr_A[i] != arr_A[i-1] or arr_B[i] != arr_B[i-1]) and arr_A[i-1] != arr_B[i-1]:
+        if (arr_A[i] != arr_A[i-1] or arr_B[i] != arr_B[i-1]) and (arr_A[i-1] != arr_B[i-1]):
            ans += 1
 
 print(ans)
