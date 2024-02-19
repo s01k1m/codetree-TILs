2add10706dda4ffa542dcc1a38f0b9bc0a1e36c7
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-20 04:16:36 +0900

    Title: 흰검 칠하기, Time: 230ms, Memory: 43MB, Status: Passed - CodeTree

--- 240220/흰검 칠하기/painting-white-black.py
+++ 240220/흰검 칠하기/painting-white-black.py
@@ -1,44 +1,71 @@
 max_step = 100
 max_order = 1000
 
-arr = [0] * (max_order * max_step * 2 + 1)
-color = [0] * (max_order * max_step * 2 + 1)
+class Tile:
+    def __init__(self, last_color = ""):
+        self.last_color = last_color
+        self.cnt_w = 0
+        self.cnt_b = 0
+
+    def white(self):
+        self.cnt_w += 1
+
+    def black(self):
+        self.cnt_b += 1
+    
+    def w(self):
+        return self.cnt_w
+
+    def b(self):
+        return self.cnt_b
+
+color = list( Tile() for _ in range(max_order * max_step * 2 + 1))
+
 n = int(input())
 cur = 1000
+segments = []
+
 for _ in range(n):
     step, direction = input().split()
     step = int(step)
+
     if direction == "R":
-        nxt = cur + step
+        nxt = cur + step - 1 # 현재 자리를 칠한다는 것을 반영함 즉 한칸 덜감
+        segments.append((cur, nxt + 1, "B")) # +1은 위에서 현재를 포함하는 바람에 한 것임
 
-        for i in range(cur, nxt, +1):
-
-            arr[i] += 1
-            color[i] = "B"
-
-        cur = nxt - 1
+        cur = nxt
 
     elif direction == "L":
-        nxt = cur - step
+        nxt = cur - step + 1 # 현재 자리를 칠한다는 것을 반영함
+        segments.append((nxt, cur + 1, "W"))
 
-        for i in range(cur, nxt, -1):
-            arr[i] += 1
-            color[i] = "W"
-        
-        cur = nxt +1
+        cur = nxt
 
+for x, y, c in segments:
+    for i in range(x, y):
+        if c == "B":
+            color[i].last_color = "B"
+            color[i].black()
+        else:
+            color[i].white()
+            color[i].last_color = "W"
 
 gray = 0
 white = 0
-black = 0  
+black = 0
+
 for e in range(len(color)):
-    if arr[e] == 0:
+    # break 
+    l_w = color[e].w()
+    l_b = color[e].b()
+
+    if color[e].last_color == "":
         continue
-    elif arr[e] >= 4:
+    elif l_b >= 2 and l_w >= 2:
         gray += 1
-    elif color[e] == "W":
+    elif color[e].last_color == "W":
         white += 1
-    elif color[e] == "B" :
+    elif color[e].last_color == "B" :
         black += 1
 
 
