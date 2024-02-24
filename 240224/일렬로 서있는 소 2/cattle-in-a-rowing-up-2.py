61395b957f875a3b70a2aee69150e4303995bfc0
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-24 23:13:26 +0900

    Title: 일렬로 서있는 소 2, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240224/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,20 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+ans = 0
+
+for i in range(n):
+    cnt =1
+    last = arr[i]
+    for j in range(i+1, n):
+        if last <= arr[j]:
+            cnt += 1
+            last = arr[j]
+            for k in range(j+1, n):
+                if last <= arr[k]:
+                    cnt += 1
+                    if cnt == 3:
+                        ans += 1
+                        cnt = 2
+                        continue
+print(ans)
