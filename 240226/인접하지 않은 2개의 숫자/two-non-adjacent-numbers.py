2372c512b6ee158933e050aaf1b3d849f75bf1d3
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-26 06:32:54 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

--- 240226/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
+++ 240226/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
@@ -2,7 +2,7 @@ n = int(input())
 arr = list(map(int, input().split()))
 max_s = 0
 for i in range(n-2):
-    for j in range(i+1, n):
+    for j in range(i+2, n):
         s = arr[i] + arr[j]
         max_s = max(max_s,s)
 
