c98f0ca14c7405682c9508b2a1a0ca1f2e535cfc
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-27 23:58:25 +0900

    Title: Carry 피하기 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240227/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,37 @@
+n = int(input())
+
+arr = [
+    input()
+    for _ in range(n)
+]
+ans = []
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            a = int(arr[i])
+            b = int(arr[j])
+            c = int(arr[k])
+
+            flag = True
+            t = 10000
+                        
+            while t >= 1:
+
+                s = a // t + b // t + c // t
+                a = a % t
+                b = b % t
+                c = c % t
+
+                if s >= 10:
+                    flag = False
+                    break
+
+                t /= 10
+            
+            if t == 1 and flag:
+                ans.append(int(arr[i])+int(arr[j])+int(arr[k]))
+
+if ans:
+    print(max(ans))
+else:
+    print(-1)
