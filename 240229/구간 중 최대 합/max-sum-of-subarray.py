b5e1a78571e1d073e9b4fbc0409359a875448d08
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-29 00:07:23 +0900

    Title: 구간 중 최대 합, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240229/구간 중 최대 합/max-sum-of-subarray.py
+++ 240229/구간 중 최대 합/max-sum-of-subarray.py
@@ -1,7 +1,8 @@
 n, k = map(int,input().split())
 arr = list(map(int, input().split()))
 ans = 0
-for i in range(n-k):
+for i in range(n-k+1):
     s = sum(arr[i:i+k])
+    # print(arr[i:i+k])
     ans = max(ans,s)
 print(ans)
