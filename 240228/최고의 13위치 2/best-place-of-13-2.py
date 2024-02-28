557962ab4e381778f014767bb093e433d9af8b39
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-02-28 23:00:47 +0900

    Title: 최고의 13위치 2, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240228/최고의 13위치 2/best-place-of-13-2.py
+++ 240228/최고의 13위치 2/best-place-of-13-2.py
@@ -6,12 +6,19 @@ arr = [
 ]
 
 ans = []
+def in_range(x,y):
+    return 0 <= x, y, y+1, y+2 < N 
 
 for i in range(N):
     for j in range(N-2):
-        s = arr[i][j] + arr[i][j+1] + arr[i][j+2]
-        ans.append(s)
+        for k in range(N):
+            for v in range(N-2):
+                if i == k and j <= v <= j+2:
+                    continue
+                elif in_range(i,j) and in_range(k, v): 
+                    s = (arr[i][j] + arr[i][j+1] + arr[i][j+2])+ (arr[k][v]+arr[k][v+1] + arr[k][v+2])
+                    ans.append(s)
 
 ans.sort(reverse=True)
 
-print(ans[0]+ ans[1])
+print(ans[0])
