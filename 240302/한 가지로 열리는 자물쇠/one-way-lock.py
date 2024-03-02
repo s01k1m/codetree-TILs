95a69ba6f20e5a21eac017e892cc47e1a7fd70d2
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-03-02 23:37:41 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 188ms, Memory: 26MB, Status: Passed - CodeTree

--- 240302/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 240302/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -5,7 +5,8 @@ ans = 0
 for i in range(1, N+1):
     for j in range(1, N+1):
         for k in range(1, N+1):
-            if abs(i-arr[0])<=2 or abs(j-arr[1]) <=2 or abs(k-arr[2]):
+            if abs(i-arr[0]) <= 2 or abs(j-arr[1]) <=2 or abs(k-arr[2]) <= 2:
                 ans += 1
+
 
 print(ans)
