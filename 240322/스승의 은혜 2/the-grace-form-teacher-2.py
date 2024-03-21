166a9927f0207a9d25161ef43c477b0b2fdc0a5e
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-03-22 00:56:11 +0900

    Title: 스승의 은혜 2, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240322/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,30 @@
+# 반값 쿠폰 1개를 사용해서 선물 가능한 학생의 최대 명수를 구하기
+N, B = map(int, input().split())
+arr = [
+    int(input())
+    for _ in range(N)
+]
+
+# 최대한 많은 학생들을 선물하기 위해서 작은 가격부터 정렬한다
+arr.sort()
+
+ans = 0
+# 반값 쿠폰 적용 완전탐색 시작
+
+for idx in range(N):
+    # print(arr)
+    arr[idx] = arr[idx] // 2 # 반값 쿠폰 적용 참고, 선물의 가격은 항상 짝수입니다
+
+    temp_students = 0 # arr[idx]에 반값 쿠폰 적용했을 때 선물 가능한 학생 수
+    temp_sum = 0 # 그 학생들한테 선물했을 때 총 비용 
+    start = 0 # 첫번째 학생 인덱스
+    while temp_sum < B: # 총 선물 비용이 최대 비용보다 작은 동안에는
+        temp_students += 1 # index 번째 학생 선물한다
+        temp_sum += arr[start] # 총비용에 더한다
+        start += 1 # 다음 학생으로 넘어갈 준비를 한다
+    
+    ans = max(ans, temp_students)
+
+    arr[idx] *= 2 # 선물비용 쿠폰 적용했던 것 원상 복구
+
+print(temp_students)
