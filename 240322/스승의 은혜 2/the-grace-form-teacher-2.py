b67e6776c050d4fb8a2f5390041033bee6769931
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-03-22 01:03:00 +0900

    Title: 스승의 은혜 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240322/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240322/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -5,9 +5,6 @@ arr = [
     for _ in range(N)
 ]
 
-# 최대한 많은 학생들을 선물하기 위해서 작은 가격부터 정렬한다
-arr.sort()
-
 ans = 0
 # 반값 쿠폰 적용 완전탐색 시작
 
@@ -16,10 +13,12 @@ for idx in range(N):
     arr[idx] = arr[idx] // 2 # 반값 쿠폰 적용 참고, 선물의 가격은 항상 짝수입니다
     temp_arr = arr[::] # 최대한 많은 학생들을 선물하기 위해서 작은 가격부터 정렬한다
     temp_arr.sort()
+
     temp_students = 0 # arr[idx]에 반값 쿠폰 적용했을 때 선물 가능한 학생 수
     temp_sum = 0 # 그 학생들한테 선물했을 때 총 비용 
     start = 0 # 첫번째 학생 인덱스
-    while (B-temp_sum >= temp_arr[start]): # 총 선물 비용이 최대 비용보다 작고 남은 금액이 더할 학생 보다 크거나 작은 동안에는
+
+    while start < N and (B - temp_sum ) >= temp_arr[start]: # 총 선물 비용이 최대 비용보다 작고 남은 금액이 더할 학생 보다 크거나 작은 동안에는
             temp_students += 1 # index 번째 학생 선물한다
             temp_sum += temp_arr[start] # 총비용에 더한다
             start += 1 # 다음 학생으로 넘어갈 준비를 한다
