56d21c749c8f8ff9eba952d5af5cd4651dfa6ffa
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-04-02 23:52:48 +0900

    Title: 등차수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240402/등차수열/arithmetic-sequence.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr = map(int, input().split())
+MAX_A = 100
+ans = 0
+
+# 각 숫자에 대해 
+# 등차수열의 개수를 확인합니다.
+for x in range(1, MAX_A + 1):
+    # 모든 쌍을 만들어 등차수열의 개수를 확인합니다.
+    cnt = 0
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if arr[i] + arr[j] == 2 * x:
+                cnt += 1
+
+    ans = max(ans, cnt)
+
+print(ans)
