a09799de31ce13477a5e5fc910f2ed66c58e3bd0
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-04-21 22:58:54 +0900

    Title: 아름다운 수, Time: 319ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240421/아름다운 수/beautiful-number.py
@@ -0,0 +1,50 @@
+# 1~4 이하의 숫자로만 이루어져있따.
+# n자리의 아름다운 숫자를 구하라.
+# 조건1: 숫자가 숫자의 크기만큼 연달아 나오는 수
+# 조건2: 동일한 숫자 묶음이 연달아 나오는 
+
+# 숫자묶음 [[1], [2,2], [3,3,3], [4,4,4,4]]
+
+# 1 -> 1
+# 3 -> 111, 122, 221, 333
+
+# 변수 선언 및 입력:
+n = int(input())
+ans = 0
+seq = list()
+
+
+def is_beautiful():
+    # 연달아 같은 숫자가 나오는 시작 위치를 잡습니다.
+    i = 0
+    while i < n:
+        # 만약 연속하여 해당 숫자만큼 나올 수 없다면
+        # 아름다운 수가 아닙니다.
+        if i + seq[i] - 1 >= n:
+            return False
+        # 연속하여 해당 숫자만큼 같은 숫자가 있는지 확인합니다.
+        # 하나라도 다른 숫자가 있다면
+        # 아름다운 수가 아닙니다.
+        for j in range(i, i + seq[i]):
+            if seq[j] != seq[i]:
+                return False
+            
+        i += seq[i]
+        
+    return True
+
+def count_beautiful_seq(cnt):
+    global ans
+    
+    if cnt == n:
+        if is_beautiful():
+            ans += 1
+        return
+	
+    for i in range(1, 5):
+        seq.append(i)
+        count_beautiful_seq(cnt + 1)
+        seq.pop()
+
+count_beautiful_seq(0)
+print(ans)
