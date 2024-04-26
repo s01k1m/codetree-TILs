77664e6502ba2d3dcaa85842ad1191f036cf2d04
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-04-26 22:53:33 +0900

    Title: 3개의 선 2, Time: 739ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240426/3개의 선 2/three-lines-2.py
@@ -0,0 +1,52 @@
+import copy
+
+
+N = int(input())
+arr = [
+    tuple(map(int, input().split()))
+    for _ in range(N)
+]
+arr.sort()
+
+def straing(number, array, cnt):
+    global ans
+    global arr
+    newArr = copy.deepcopy(array)
+
+    if (len(newArr)== 0):
+        ans = True
+        return
+    
+    if cnt == 3:
+        return
+    else:
+        a = number[cnt]
+
+        for idx, item in enumerate(newArr):
+            if item[0] == a:
+                newArr.pop(idx)
+        
+        straing(number, newArr, cnt+1)
+
+        newArr = copy.deepcopy(arr)
+        for idx, item  in enumerate(newArr):
+            if item[1] == a:
+                newArr.pop(idx)
+        straing(number, newArr, cnt+1)
+
+
+ans = False
+
+for i in range(0,10):
+    for j in range(0,10):
+        for k in range(0,10):
+            # (1, x) (x, 0) (x, 4)
+
+
+            number = [i,j,k]
+            straing(number, arr, 0)
+
+if arr:
+    print(1)
+else:
+    print(0)
