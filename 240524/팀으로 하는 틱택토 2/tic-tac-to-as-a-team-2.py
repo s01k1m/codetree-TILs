0c083224bf3523238c694908b884c103547871e0
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-05-24 12:29:38 +0900

    Title: 팀으로 하는 틱택토 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240524/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240524/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -3,20 +3,25 @@ arr = [
     for _ in range(3)
 ]
 # 팀으로 이겼다는 뜻은 한 줄에 팀을 이루고 있는 2개의 숫자가 적어도 하나씩은 등장해야함
-ans = set([])
+# ans = set([])
+ans = 0
 def win(a, b, c):
     global ans
     set_list = list(set([a, b, c]))
     if a == b and len(set_list) == 2:
-        ans.add(a)
+        # ans.add(a)
+        ans +=1 
     elif a == c and len(set_list) == 2:
-        ans.add(a)
+        ans +=1 
+        # ans.add(a)
     elif b == c and len(set_list) == 2:
-        ans.add(b)
+        ans +=1 
+        # ans.add(b)
 for i in range(0, 3):
     win(arr[i][0],arr[i][1],arr[i][2])
     win(arr[0][i], arr[1][i], arr[2][i])
 win(arr[0][0], arr[1][1], arr[2][2])
 win(arr[2][0], arr[1][1], arr[0][2])
 
-print(len(ans))
+# print(len(set(ans)))
+print(ans)
