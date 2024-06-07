e1f1a53ebcf7cb3b47c41f1330a3c3ef9a2ef80b
Sol Kim <121751626+s01k1m@users.noreply.github.com>
2024-06-08 00:04:26 +0900

    Title: 문자열 바꾸기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240608/문자열 바꾸기/replace-string.py
@@ -0,0 +1,21 @@
+char1 = list(input())
+char2 = list(input())
+ans = 0
+
+c1 = sorted(char1)
+c2 = sorted(char2)
+
+def isMatched(p1, p2):
+    global ans
+    if p1 != p2:
+        ans +=1
+
+
+if c1 != c2:
+    print(-1)
+
+else:
+    for i in range(len(char1)):
+        isMatched(char1[i], char2[i])
+
+    print(ans)
